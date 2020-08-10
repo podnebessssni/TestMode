3e7a3e240f6f5dad11c68f89b2a85a690fb8ca8a
Oleg Georgiyevski <oleqg@yahoo.com>
2020-08-10 08:04:59 +0300

    Code Structured

---
+++ src/test/java/ru/netology/domain/Registration.java
@@ -0,0 +1,12 @@
+package ru.netology.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class Registration {
+    private String login;
+    private String password;
+    private String status;
+}
